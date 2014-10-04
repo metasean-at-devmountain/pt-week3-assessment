99161ad266157b95bb8d06b9ca5162e53e5726d3
MetaSean <metasean@users.noreply.github.com>
2014-10-04 12:57:13 -0600

    callbacks.js completed

--- callbacks.js
+++ callbacks.js
@@ -3,6 +3,14 @@
   The code below should log "The reversedString is .desrever teg dluohs sihT"
 */
 var sentence = 'This should get reversed.'
+
+var reverse = function (str, callback) {
+	var arr = [];
+  arr = str.split('');
+	arr.reverse();
+	callback (arr.join(''));
+};
+
 reverse(sentence, function(reversedString){
   console.log('The reversedString is ' + reversedString);
 });
